4b5489c07a39331ce84cdf5c6efc38bf7e70a457
Aymenhammoudaa <168995647+Aymenhammoudaa@users.noreply.github.com>
2024-11-09 18:28:23 +0100

    add myfiles

--- src/main/java/tn/esprit/tpfoyer/repository/EtudiantRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.tpfoyer.repository;
-
-import tn.esprit.tpfoyer.entity.Etudiant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
-}
